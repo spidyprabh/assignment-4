cdb09c783b3a5e9e332d82c72b465427b7606d14
prabhjyot <psingh1402@conestogac.on.ca>
2018-12-04 01:45:35 -0500

    Validations For Creating a record

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,65 @@
 class User < ApplicationRecord
+  validates(
+    :email,
+    presence: true,
+    length: {
+      maximum: 255
+    },
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  )
+
+  validates(
+    :name,
+    presence: true,
+    length: {
+      maximum: 255
+    }
+  )
+
+  validates(
+    :name,
+    presence: true,
+    length: {
+      maximum: 255
+    }
+  )
+
+  validates(
+    :phone,
+    presence: true,
+    length: {
+      maximum: 255
+    },
+    format: { with: /((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/ }
+  )
+
+  validates(
+    :address,
+    presence: true
+  )
+
+  validates(
+    :city,
+    presence: true
+  )
+
+  validates(
+    :vehicle_make,
+    presence: true
+  )
+
+  validates(
+    :vehicle_model,
+    presence: true
+  )
+
+  validates(
+    :vehicle_year,
+    presence: true,
+    numericality: true
+  )
+
+  def get_url
+    "https://www.jdpower.com/Cars/#{self.vehicle_year}/#{self.vehicle_make}/#{self.vehicle_model}"
+  end
 end
